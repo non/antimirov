d1ce5da222ad4e2631641c07f40d3d51795a2637
Erik Osheim <d_m@plastic-idolatry.com>
2020-06-22 21:54:49 -0400

    remove timing from nfa/dfa tests

--- tests/jvm/src/test/scala/antimirov/DfaTest.scala
+++ tests/jvm/src/test/scala/antimirov/DfaTest.scala
@@ -5,37 +5,36 @@ import org.typelevel.claimant.Claim
 
 import Util._
 
-object DfaTest extends Properties("DfaTest") with TimingProperties { self =>
+object DfaTest extends Properties("DfaTest") { self =>
 
   override def overrideParameters(params: Test.Parameters): Test.Parameters =
     params
       .withMinSuccessfulTests(500)
       //.withPropFilter(Some("dfa regression #3"))
 
-  override def scale: Long = 20L
-  override def enableTiming = true
-  override def failOnAbort = false
+  property("dfa accepts regex strings") =
+    Prop.forAll(genRxAndStrs) { case (rx, set) =>
+      val dfa = rx.toDfa
+      set.iterator.map { s =>
+        val lhs = rx.accepts(s)
+        val rhs = dfa.accepts(s)
+        Claim(lhs == rhs) :| s"failed to agree on '$s'"
+      }.foldLeft(Prop(true))(_ && _)
+    }
 
-  timedProp("dfa accepts regex strings", genRxAndStrs) { case (rx, set) =>
-    val dfa = rx.toDfa
-    set.iterator.map { s =>
-      val lhs = rx.accepts(s)
-      val rhs = dfa.accepts(s)
-      Claim(lhs == rhs) :| s"failed to agree on '$s'"
-    }.foldLeft(Prop(true))(_ && _)
-  }
+  property("accepts = !rejects") =
+    Prop.forAll(genRxAndStrs) { case (rx, set) =>
+      val dfa = rx.toDfa
+      set.iterator.map { s =>
+        Claim(dfa.rejects(s) != dfa.accepts(s))
+      }.foldLeft(Prop(true))(_ && _)
+    }
 
-  timedProp("accepts = !rejects", genRxAndStrs) { case (rx, set) =>
-    val dfa = rx.toDfa
-    set.iterator.map { s =>
-      Claim(dfa.rejects(s) != dfa.accepts(s))
-    }.foldLeft(Prop(true))(_ && _)
-  }
-
-  timedProp("(x = y) -> (x.toString = y.toString)", genRx, genRx) { (rx1, rx2) =>
-    val (dfa1, dfa2) = (rx1.toDfa, rx2.toDfa)
-    Claim((dfa1 != dfa2) || (s"$dfa1" == s"$dfa2"))
-  }
+  property("(x = y) -> (x.toString = y.toString)") =
+    Prop.forAll(genRx, genRx) { (rx1, rx2) =>
+      val (dfa1, dfa2) = (rx1.toDfa, rx2.toDfa)
+      Claim((dfa1 != dfa2) || (s"$dfa1" == s"$dfa2"))
+    }
 
   property("dfa regression #1") =
     Claim(Rx.parse("[ab]b").toDfa.rejects("aa"))
--- tests/jvm/src/test/scala/antimirov/NfaTest.scala
+++ tests/jvm/src/test/scala/antimirov/NfaTest.scala
@@ -5,41 +5,41 @@ import org.typelevel.claimant.Claim
 
 import Util._
 
-object NfaTest extends Properties("NfaTest") with TimingProperties { self =>
+object NfaTest extends Properties("NfaTest") { self =>
 
   override def overrideParameters(params: Test.Parameters): Test.Parameters =
     params
       .withMinSuccessfulTests(500)
       //.withPropFilter(Some("regression"))
 
-  override def scale: Long = 20L
-  override def enableTiming = true
-  override def failOnAbort = false
+  property("nfa accepts regex strings") =
+    Prop.forAll(genRxAndStrs) { case (rx, set) =>
+      val nfa = rx.toNfa
+      set.iterator.map { s =>
+        val lhs = rx.accepts(s)
+        val rhs = nfa.accepts(s)
+        Claim(lhs == rhs) :| s"failed to accept '$s'"
+      }.foldLeft(Prop(true))(_ && _)
+    }
 
-  timedProp("nfa accepts regex strings", genRxAndStrs) { case (rx, set) =>
-    val nfa = rx.toNfa
-    set.iterator.map { s =>
-      val lhs = rx.accepts(s)
-      val rhs = nfa.accepts(s)
-      Claim(lhs == rhs) :| s"failed to accept '$s'"
-    }.foldLeft(Prop(true))(_ && _)
-  }
+  property("builer toString") =
+    Prop.forAll(genRx) { rx =>
+      Claim(Nfa.Builder.fromRx(rx).toString != "")
+    }
 
-  timedProp("builer toString", genRx) { rx =>
-    Claim(Nfa.Builder.fromRx(rx).toString != "")
-  }
+  property("accepts = !rejects") =
+    Prop.forAll(genRxAndStrs) { case (rx, set) =>
+      val nfa = rx.toNfa
+      set.iterator.map { s =>
+        Claim(nfa.rejects(s) != nfa.accepts(s))
+      }.foldLeft(Prop(true))(_ && _)
+    }
 
-  timedProp("accepts = !rejects", genRxAndStrs) { case (rx, set) =>
-    val nfa = rx.toNfa
-    set.iterator.map { s =>
-      Claim(nfa.rejects(s) != nfa.accepts(s))
-    }.foldLeft(Prop(true))(_ && _)
-  }
-
-  timedProp("(x = y) -> (x.toString = y.toString)", genRx, genRx) { (rx1, rx2) =>
-    val (nfa1, nfa2) = (rx1.toNfa, rx2.toNfa)
-    Claim((nfa1 != nfa2) || (s"$nfa1" == s"$nfa2"))
-  }
+  property("(x = y) -> (x.toString = y.toString)") =
+    Prop.forAll(genRx, genRx) { (rx1, rx2) =>
+      val (nfa1, nfa2) = (rx1.toNfa, rx2.toNfa)
+      Claim((nfa1 != nfa2) || (s"$nfa1" == s"$nfa2"))
+    }
 
   property("nfa regression #1") =
     Claim(Rx.parse("[ab]b").toNfa.rejects("aa"))
